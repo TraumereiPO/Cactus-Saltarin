b163bc00a4ec822f0a365460e72e656ca5f49f45
Traumerei <kerenlarios511@gmail.com>
2025-05-09 11:58:58 -0600

    Aqui esta la actualizacion
    El cactus ya salta y se agacha : - ) 👍 y
    tambien  agregue la animación del dinosaurio. Mel el dinosaurio solo tenes que agregar el codigo. trabaja con bing y algun video para que sea mas facil

--- captus_saltarin-3.0/personaje_captus.gd
+++ captus_saltarin-3.0/personaje_captus.gd
@@ -1,18 +1,34 @@
 extends CharacterBody2D
 
+const SPEED = 500.0
+const JUMP_VELOCITY = -700.0
+const GRAVITY = 1600.0
+const CROUCH_OFFSET = 10.0
+@onready var animated_sprite = $AnimatedSprite
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var is_crouching = false
+var defauld_position_y
+func _ready():
+	defauld_position_y = position.y
+	animated_sprite.play("reposo")
 
-
-func _physics_process(delta: float) -> void:
+func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y += GRAVITY * delta
+	#manejo del agachado
+	if Input.is_action_just_pressed("ui_down") and is_on_floor() and not is_crouching:
+			is_crouching = true
+			animated_sprite.play("Agachar")
+			position.y += CROUCH_OFFSET
+			
+	elif Input.is_action_just_released("ui_down") and is_crouching:
+		is_crouching = false
+		animated_sprite.play("reposo")
+		position.y = defauld_position_y
+	# salta solo si no esta agachado
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and not is_crouching:
 		velocity.y = JUMP_VELOCITY
+		animated_sprite.play("Saltar")
 
-
-	move_and_slide()
+	move_and_slide() 
